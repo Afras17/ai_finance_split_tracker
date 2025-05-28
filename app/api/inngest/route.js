02796165ad7412080464cd8bf5edb9398c921354
Afras Rahim <afrasrahim11@gamil.com>
2025-05-28 13:38:56 +0530

    resend email done

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,11 +1,10 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/lib/inngest/client";
-import { helloWorld } from "@/lib/inngest/functions";
+import { paymentReminders } from "@/lib/inngest/payment-reminders";
 
 export const { GET, POST, PUT } = serve({
     client: inngest,
     functions: [
-     /* your functions will be passed here later! */
-     helloWorld,
+        paymentReminders,
     ],
 })
